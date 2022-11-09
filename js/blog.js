bc304be12bf460fbb3786a9516d5616fdadd482e
Tonix89 <Antonio.Arabejo@stud.noroff.no>
2022-11-09 20:17:14 +0100

    blog page updated

--- js/blog.js
+++ js/blog.js
@@ -2,7 +2,7 @@ import { message } from "./message/message.js";
 
 const latestPost = document.querySelector(".blg_post_cont");
 const showMore = document.querySelector(".show_more");
-console.log(showMore);
+const hideAll = document.querySelector(".hide_all");
 
 const url = "https://tonix.site/daily-devotion/wp-json/wp/v2/posts";
 console.log(url);
@@ -71,10 +71,54 @@ async function gravatarApi(pic, post) {
         </div>
     </div>`;
 
-    const posted = document.querySelectorAll(".blg_mn_pst_cntnr");
-    console.log(posted);
+    hidePost();
+    hiddenPost();
   } catch (error) {
     console.log(error);
     latestPost.innerHTML = message("error", error);
+  }
+}
+
+function hidePost() {
+  const posted = document.querySelectorAll(".blg_mn_pst_cntnr");
+  console.log(posted);
+  console.log(hideAll);
+  hideAll.className = "hideBtn";
+
+  for (let i = 0; i < posted.length; i++) {
+    if (i >= 1) {
+      posted[i].className = "blg_hide";
+    }
+  }
+}
+
+function hiddenPost() {
+  let y = 0;
+  console.log(showMore);
+
+  showMore.onclick = function () {
+    showPost((y += parseInt(showMore.value)));
+  };
+
+  hideAll.onclick = function () {
+    document.querySelector(".hideBtn").className = "show_more";
+    y = 0;
+    hidePost();
+  };
+
+  const blogHide = document.querySelectorAll(".blg_hide");
+  console.log(blogHide);
+
+  function showPost(x) {
+    console.log(x);
+    for (let i = 0; i < blogHide.length; i++) {
+      if (i < x) {
+        blogHide[i].className = "blg_mn_pst_cntnr";
+      }
+      if (x >= blogHide.length) {
+        document.querySelector(".hideBtn").className = "hide_all";
+        showMore.className = "hideBtn";
+      }
+    }
   }
 }
